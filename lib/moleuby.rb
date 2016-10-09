b61c5a030e2ccd3e73bed92fb18a32cd3811f4f6
wataru0225 <wataru.kikuchi0225@gmail.com>
2016-10-09 11:22:01 +0900

    [modified]change name

--- lib/moleuby.rb
+++ lib/moleuby.rb
@@ -1,52 +1,6 @@
 require "moleuby/version"
+require "moleuby/digging"
+require "moleuby/method_reading"
 
 module Moleuby
-  require "set"
-
-  class Molebox
-    def initialize(application)
-      @base = application
-      @virt = application.clone
-    end
-
-    def run(&block)
-      @virt.instance_eval &block
-    end
-
-    def locals
-      base_vars = Set.new(@base.instance_variables)
-      hash = @virt
-             .instance_variables
-             .select {|var| !(base_vars.include? var)}
-             .map    {|var| {trim(var) => dict(@virt,var)}}
-             .inject({}) {|ret,h| ret.merge(h)}
-      return Hashie::Mash.new(hash)
-    end
-
-    def method_missing(name, *args)
-      @virt.send(name,*args)
-    end
-
-    private
-
-    def dict(instance,var)
-      return instance.instance_variable_get(var)
-    end
-
-    def trim(var)
-      return var.to_s[1..-1].to_sym
-    end
-
-    def diff
-      @base.instance_variables.select do |var|
-        (dict(@base,var) == dict(@virt,var)) ? false : true
-      end
-    end
-  end
-
-  def self.digging(&block)
-    @digging ||= Molebox.new(self)
-    @digging.run &block if block
-    return @digging
-  end
 end
